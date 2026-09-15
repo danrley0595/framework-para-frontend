2e89e04d24cf21a23060dad5b003a519ad3ab24f
danrley0595 <danrleycramos@gmail.com>
2026-09-15 13:03:09 -0300

    Add files via upload

---
+++ Aula04/Atividade/script.js
@@ -0,0 +1,4 @@
+document.getElementById('botaoContato').addEventListener('click', function () {
+  document.getElementById('contato').textContent =
+    'Telefone: 62-99214**** | E-mail: teste@teste.com';
+});
